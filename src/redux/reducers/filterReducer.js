7701ea4c76eeccb6660448442299dadebc52350e
neel123go <ayounpaul1029@gmail.com>
2022-12-11 14:57:05 +0600

    Implement filter for product

--- src/redux/reducers/filterReducer.js
+++ src/redux/reducers/filterReducer.js
@@ -1,9 +1,44 @@
+import { TOGGLE_BRAND, TOGGLE_STOCK } from "../actionTypes/actionTypes";
+
 const initialState = {
-    test: 'TEST'
+    filters: {
+        brands: [],
+        stock: false,
+    },
+    keywords: '',
 };
 
 const filterReducer = (state = initialState, action) => {
-    return state;
+    switch (action.type) {
+        case TOGGLE_BRAND:
+            if (!state.filters.brands.includes(action.payload)) {
+                return {
+                    ...state,
+                    filters: {
+                        ...state.filters,
+                        brands: [...state.filters.brands, action.payload],
+                    },
+                }
+            } else {
+                return {
+                    ...state,
+                    filters: {
+                        ...state.filters,
+                        brands: state.filters.brands.filter(product => product !== action.payload),
+                    },
+                }
+            }
+        case TOGGLE_STOCK:
+            return {
+                ...state,
+                filters: {
+                    ...state.filters,
+                    stock: !state.filters.stock,
+                },
+            }
+        default:
+            return state;
+    }
 }
 
 export default filterReducer;
